c2118553b09a53f537ca64a9bca1fd1c66fa7e84
Bernhard Holl <hollei@hotmail.com>
2026-04-04 13:11:06 +0200

    updats

---
+++ src/xw_studio/services/sendungen/__init__.py
@@ -0,0 +1,5 @@
+"""Offene-Sendungen service package."""
+
+from xw_studio.services.sendungen.service import OffeneSendungenService, SendungCase
+
+__all__ = ["OffeneSendungenService", "SendungCase"]
